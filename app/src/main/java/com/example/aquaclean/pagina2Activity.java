99e8accf461ee572e03e3a498e2e1b48f9c2f778
Igor Gutierrez de Carvalho Lopes <82417888+IgorGuti@users.noreply.github.com>
2023-08-22 22:36:48 -0300

    Atualização da pagina 1, com botão operando para pagina de login.

---
+++ app/src/main/java/com/example/aquaclean/pagina2Activity.java
@@ -0,0 +1,13 @@
+package com.example.aquaclean;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class pagina2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagina2); // Define o layout da segunda atividade
+    }
+}
